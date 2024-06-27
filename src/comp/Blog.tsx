0923adf2a18dfe527e6a3b92e459adf92d4177af
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-27 21:52:32 +0200

    adding blog

---
+++ src/comp/Blog.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export interface BlogItem {
+  url: string;
+  id: string;
+  title: string;
+  likes: number;
+  author: string;
+}
+
+interface BlogProps {
+  blogItem: BlogItem;
+  handelLike: (id: string) => void;
+}
+
+export default function Blog({ blogItem, handelLike }: BlogProps) {
+  return (
+    <div className="flex size-60 flex-col justify-center gap-4 border-2 border-white">
+      <h2>{blogItem.title}</h2>
+      <span>{blogItem.author}</span>
+      <span></span>
+
+      <span>{blogItem.url}</span>
+
+      <span>{blogItem.likes}</span>
+      <button onClick={() => handelLike(blogItem.id)}>Like +</button>
+    </div>
+  );
+}
