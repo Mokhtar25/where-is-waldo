fae42662b1e287e5adbff27f11e4d07a8cb340b4
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-18 18:10:05 +0200

    changing the way to handel clicks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { MouseEvent, MouseEventHandler, useRef, useState } from "react";
 import Model from "./comp/model";
 
 interface mouseCord {
@@ -11,33 +11,38 @@ const App = () => {
   const [y, setY] = useState<mouseCord>();
   const [model, setModel] = useState(true);
 
-  const ref = useRef<HTMLDivElement | null>(null);
-  const handelMouse = (e: MouseEvent) => {
-    const bor = ref.current?.getBoundingClientRect();
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+  // const ref = useRef<HTMLDivElement | null>(null);
+  // const handelMouse = (e: MouseEvent) => {
+  //   const bor = ref.current?.getBoundingClientRect();
+  //   const width = window.innerWidth;
+  //   const height = window.innerHeight;
+  //
+  //   if (bor) {
+  //     if (
+  //       e.x <= bor?.width + bor?.x &&
+  //       e.x >= bor?.x &&
+  //       e.y <= bor?.height + bor?.y &&
+  //       e.y >= bor.y
+  //     )
+  //       return null;
+  //   }
+  //   // adjust the value to height and length of the screen
+  //   const x = ((width / e.x) * 1000).toFixed(0);
+  //   const y = ((height / e.y) * 1000).toFixed(0);
+  //   setX({
+  //     page: e.pageX,
+  //     offset: parseInt(x),
+  //   });
+  //   setY({
+  //     page: e.pageY,
+  //     offset: parseInt(y),
+  //   });
+  // };
+  // document.onmousedown = handelMouse;
 
-    if (
-      e.x <= bor?.width + bor?.x &&
-      e.x >= bor?.x &&
-      e.y <= bor?.height + bor?.y &&
-      e.y >= bor.y
-    )
-      return null;
-    console.log(bor?.x, e.x, bor?.width, bor?.left, bor?.width + bor?.x);
-    // adjust the value to height and length of the screen
-    const x = ((width / e.x) * 1000).toFixed(0);
-    const y = ((height / e.y) * 1000).toFixed(0);
-    setX({
-      page: e.pageX,
-      offset: parseInt(x),
-    });
-    setY({
-      page: e.pageY,
-      offset: parseInt(y),
-    });
+  const imgClick = (event: React.MouseEvent<HTMLImageElement>) => {
+    console.log(event.nativeEvent.pageX);
   };
-  document.onmousedown = handelMouse;
 
   return (
     <div className="h-screen w-full">
@@ -49,7 +54,7 @@ const App = () => {
         open={model}
         style={{ left: `${x?.page}px`, top: `${y?.page}px` }}
       ></Model>
-      <div className="absolute right-2/4 top-2/4 size-1 rounded-full bg-secondary"></div>
+      <img src="../public/sea.gif" alt="" onClick={imgClick} />
     </div>
   );
 };
