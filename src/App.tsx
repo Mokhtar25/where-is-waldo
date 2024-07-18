893d788fd6ca8f9adefb6dbcaacee7f54c194a1f
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-18 18:57:52 +0200

    changing click to be within the image tag

--- src/App.tsx
+++ src/App.tsx
@@ -11,37 +11,26 @@ const App = () => {
   const [y, setY] = useState<mouseCord>();
   const [model, setModel] = useState(true);
 
-  // const ref = useRef<HTMLDivElement | null>(null);
-  // const handelMouse = (e: MouseEvent) => {
-  //   const bor = ref.current?.getBoundingClientRect();
-  //   const width = window.innerWidth;
-  //   const height = window.innerHeight;
-  //
-  //   if (bor) {
-  //     if (
-  //       e.x <= bor?.width + bor?.x &&
-  //       e.x >= bor?.x &&
-  //       e.y <= bor?.height + bor?.y &&
-  //       e.y >= bor.y
-  //     )
-  //       return null;
-  //   }
-  //   // adjust the value to height and length of the screen
-  //   const x = ((width / e.x) * 1000).toFixed(0);
-  //   const y = ((height / e.y) * 1000).toFixed(0);
-  //   setX({
-  //     page: e.pageX,
-  //     offset: parseInt(x),
-  //   });
-  //   setY({
-  //     page: e.pageY,
-  //     offset: parseInt(y),
-  //   });
-  // };
-  // document.onmousedown = handelMouse;
+  const ref = useRef<HTMLImageElement | null>(null);
 
   const imgClick = (event: React.MouseEvent<HTMLImageElement>) => {
-    console.log(event.nativeEvent.pageX);
+    const e = event.nativeEvent;
+
+    if (!ref.current) return;
+
+    const width = ref.current.width;
+    const height = ref.current.height;
+
+    const x = ((width / e.x) * 1000).toFixed(0);
+    const y = ((height / e.y) * 1000).toFixed(0);
+    setX({
+      page: e.pageX,
+      offset: parseInt(x),
+    });
+    setY({
+      page: e.pageY,
+      offset: parseInt(y),
+    });
   };
 
   return (
@@ -50,11 +39,10 @@ const App = () => {
         hello, world {x?.offset} {y?.offset}
       </div>
       <Model
-        refx={ref}
         open={model}
         style={{ left: `${x?.page}px`, top: `${y?.page}px` }}
       ></Model>
-      <img src="../public/sea.gif" alt="" onClick={imgClick} />
+      <img src="/sea.gif" alt="" onClick={imgClick} ref={ref} />
     </div>
   );
 };
--- src/comp/model.tsx
+++ src/comp/model.tsx
@@ -2,15 +2,13 @@ import React from "react";
 interface ModelProps {
   open: boolean;
   style: React.CSSProperties;
-  refx: any;
 }
-const Model = ({ open, style, refx }: ModelProps) => {
+const Model = ({ open, style }: ModelProps) => {
   if (!open) return null;
   return (
     <div
       style={style}
       className="absolute z-50 flex h-32 w-24 flex-col justify-between divide-y-2 divide-white bg-slate-700"
-      ref={refx}
     >
       <div>11</div>
       <div>21</div>
