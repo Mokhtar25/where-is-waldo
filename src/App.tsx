01a9b786df7f848d2624d41d851ea65939d26b87
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-18 20:20:46 +0200

    adding target machines

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,35 @@ const App = () => {
   const [x, setX] = useState<mouseCord>();
   const [y, setY] = useState<mouseCord>();
   const [model, setModel] = useState(true);
+  const cord = [{ x: 10723, y: 9598 }];
 
   const ref = useRef<HTMLImageElement | null>(null);
+  const buffer = 1500;
+
+  const submit = (type: string | null) => {
+    if (type === null) return;
+    if (!x?.offset || !y?.offset) return;
+
+    const checkX =
+      x?.offset <= cord[0].x + buffer && x?.offset >= cord[0].x - buffer;
+
+    const checkY =
+      y?.offset <= cord[0].y + buffer && y?.offset >= cord[0].y - buffer;
+    if (checkX && checkY) alert("eh");
+  };
 
   const imgClick = (event: React.MouseEvent<HTMLImageElement>) => {
     const e = event.nativeEvent;
+    setModel(!model);
 
+    const buffer = 1000;
     if (!ref.current) return;
 
     const width = ref.current.width;
     const height = ref.current.height;
 
-    const x = ((width / e.x) * 1000).toFixed(0);
-    const y = ((height / e.y) * 1000).toFixed(0);
+    const x = ((width / e.x) * buffer).toFixed(0);
+    const y = ((height / e.y) * buffer).toFixed(0);
     setX({
       page: e.pageX,
       offset: parseInt(x),
@@ -39,6 +55,7 @@ const App = () => {
         hello, world {x?.offset} {y?.offset}
       </div>
       <Model
+        submit={submit}
         open={model}
         style={{ left: `${x?.page}px`, top: `${y?.page}px` }}
       ></Model>
--- src/comp/model.tsx
+++ src/comp/model.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 interface ModelProps {
   open: boolean;
   style: React.CSSProperties;
+  submit: (type: string | null) => void;
 }
-const Model = ({ open, style }: ModelProps) => {
+const Model = ({ open, style, submit }: ModelProps) => {
   if (!open) return null;
+  const handelClick = (e: MouseEvent<HTMLDivElement>) => {
+    const type = e.currentTarget.textContent;
+    submit(type);
+  };
+
   return (
     <div
       style={style}
-      className="absolute z-50 flex h-32 w-24 flex-col justify-between divide-y-2 divide-white bg-slate-700"
+      className="absolute z-50 flex h-40 w-24 flex-col justify-between divide-y-2 divide-white rounded border-4 border-gray-500 bg-slate-700"
     >
-      <div>11</div>
-      <div>21</div>
-      <div>31</div>
+      <div
+        onClick={handelClick}
+        className="flex h-full cursor-pointer items-center justify-center transition-all hover:bg-slate-600 active:brightness-125"
+      >
+        11
+      </div>
+
+      <div
+        onClick={handelClick}
+        className="flex h-full cursor-pointer items-center justify-center transition-all hover:bg-slate-600 active:brightness-125"
+      >
+        31
+      </div>
+
+      <div
+        onClick={handelClick}
+        className="flex h-full cursor-pointer items-center justify-center transition-all hover:bg-slate-600 active:brightness-125"
+      >
+        23
+      </div>
     </div>
   );
 };
